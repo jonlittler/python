63b8f2c7c0434ae2407dd97e51f62b37b598f06c
Jon Littler <jonlittler@mac.com>
2018-06-10 09:48:04 -0400

    chapter 7 majority-vote part I

---
+++ src/MajorityVoteClassifier.py
@@ -0,0 +1,26 @@
+from sklearn.base import BaseEstimator
+from sklearn.base import ClassifierMixin
+from sklearn.base import clone
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import _name_estimators
+
+
+class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
+
+    def __init__(self, classifiers, vote='classlabel', weights=None):
+        self.classifiers = classifiers
+        self.named_classifiers = {key: value for key, value in _name_estimators(classifiers)}
+        self.vote = vote
+        self.weights = weights
+
+    def fit(self, X, y):
+        self.labelenc_ = LabelEncoder()
+        self.labelenc_.fit(y)
+        self.classes_ = self.labelenc_.classes_
+        self.classifiers_ = []
+        for clf in self.classifiers:
+            fitted_clf = clone(clf).fit(X, self.labelenc_.transform(y))
+            self.classifiers_.append(fitted_clf)
+        return self
+
+
