7e5d8e516447cbb2792a72a138342ce0a6b67ac0
Jon Littler <jonlittler@mac.com>
2018-06-25 20:55:37 -0400

    pca, lda and kernel-pca techniques

--- src/ch5/kernal-pca.py
+++ src/ch5/kernal-pca.py
@@ -91,7 +91,7 @@ def main():
     x_new = X[25]
     print(x_new)
 
-    x_proj = alphas[25] # origional projection
+    x_proj = alphas[25] # original projection
     print(x_proj)
 
     def project_x(x_new, X, gamma, alphas, lambdas):
@@ -102,8 +102,8 @@ def main():
     x_reproj = project_x(x_new, X, gamma=15, alphas=alphas, lambdas=lambdas)
     print(x_reproj)
 
-    plt.scatter(alphas[y == 0, 0], np.zeros((50)), color='red', marker='^', alpha=0.5)
-    plt.scatter(alphas[y == 1, 0], np.zeros((50)), color='blue', marker='o', alpha=0.5)
+    plt.scatter(alphas[y == 0, 0], np.zeros(50), color='red', marker='^', alpha=0.5)
+    plt.scatter(alphas[y == 1, 0], np.zeros(50), color='blue', marker='o', alpha=0.5)
     plt.scatter(x_proj, 0, color='black', label='original projection of point X[25]', marker='^', s=100)
     plt.scatter(x_reproj, 0, color='green', label='remapped point X[25]', marker='x', s=500)
     plt.legend(scatterpoints=1)
